79c0ac5b2de273035748df4da47b9a0c93f6881c
wjdeogus03 <chung.daeh@northeastern.edu>
2023-10-31 00:19:29 -0700

    finish lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -5,11 +5,16 @@ import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoItem from "./todo/TodoItem";
 import TodoList from "./todo/TodoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
+
  return (
    <div>
      <h1>Assignment 3</h1>
+     
+
      <TodoList/>
      <TodoItem/>
      <ConditionalOutput/>
