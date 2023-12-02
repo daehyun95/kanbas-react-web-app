327eb9146b017b5ce05cbee2020d5b2419741466
wjdeogus03 <chung.daeh@northeastern.edu>
2023-12-01 17:31:14 -0800

    a6

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL;
+const API_BASE = "https://kanbas-node-server-app-cwch.onrender.com";
 const USERS_API = `${API_BASE}/api/users`;
 
 
